e7128d38db0c12428d7078dc9af5bda250948528
iRuxu <rx6@qq.com>
2020-06-30 10:11:06 +0800

    update

--- src/components/activity.vue
+++ src/components/activity.vue
@@ -3,7 +3,7 @@
         <el-carousel
             v-if="data.length"
             trigger="click"
-            height="100px"
+            height="160px"
             indicator-position="none"
             :interval="6000"
         >
