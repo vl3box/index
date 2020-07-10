41fe0bf65dcee79f8b50ee44749c641050479753
iRuxu <rx6@qq.com>
2020-07-10 20:45:58 +0800

    add admin button

--- src/components/notice.vue
+++ src/components/notice.vue
@@ -3,6 +3,10 @@
         <div class="m-sideblock-header">
             <i class="el-icon-s-opportunity"></i
             ><span class="u-title">站内动态</span>
+            <span class="u-more u-admin" v-if="isSuper">
+                <a href="/admin" target="_blank" rel="noopener noreferrer">管理面板</a> | 
+                <a href="https://os.jx3box.com/admin/user/login" target="_blank" rel="noopener noreferrer">百科审核</a>
+            </span>
         </div>
         <ul class="m-sideblock-list u-list" v-if="data.length">
             <li v-for="(item, i) in data" :key="i">
@@ -22,12 +26,14 @@
 <script>
 import { getNews } from "../service/index";
 import { simpledate } from "../utils/simpledate";
+import User from '@jx3box/jx3box-common/js/user'
 export default {
     name: "notice",
     props: [],
     data: function() {
         return {
             data: [],
+            isSuper : false
         };
     },
     computed: {},
@@ -41,6 +47,9 @@ export default {
         getNews("news", 5).then((data) => {
             this.data = data;
         });
+    },
+    created : function (){
+        this.isSuper = !!(User.getInfo().group > 60)  
     },
     components: {},
 };
