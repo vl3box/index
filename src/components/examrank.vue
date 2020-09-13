6704cd0ffd8af57d683bd082970870aaac3e1425
iRuxu <rx6@qq.com>
2020-09-13 12:55:46 +0800

    试卷排行榜fix

--- src/components/examrank.vue
+++ src/components/examrank.vue
@@ -56,7 +56,7 @@ export default {
             result.forEach((item) => {
                 if (item.name.startsWith("paper")) {
                     let id = item.name.split("-").pop();
-                    list.push(id);
+                    if(!isNaN(id)) list.push(id);
                     this.views.push(item.value["7days"]);
                 }
             });
