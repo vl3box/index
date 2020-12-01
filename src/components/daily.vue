8cbfc4cfad7d97318501b3cb61ae956c20e8fcb1
iRuxu <rx6@qq.com>
2020-12-02 01:17:39 +0800

    add weibo logo

--- src/components/daily.vue
+++ src/components/daily.vue
@@ -5,6 +5,7 @@
             <img class="u-wechat" src="../assets/img/right/qrcode.png" alt="" />
             <div class="u-list">
                 <span class="u-weibo u-item">
+                    <img svg-inline src="../assets/img/weibo.svg" />
                     <em>官博：</em>
                     <a href="https://weibo.com/jx3box" target="_blank"
                         >JX3BOX魔盒</a
