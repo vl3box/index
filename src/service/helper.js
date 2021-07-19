3033fc0a4355be6ef293aaa5a7a09ad32d9945e7
iRuxu <rx6@qq.com>
2021-07-19 10:07:53 +0800

    add item price

--- src/service/helper.js
+++ src/service/helper.js
@@ -1,7 +1,13 @@
 import { $helper } from "@jx3box/jx3box-common/js/https";
 
-function getSchoolRank(v){
-    return $helper().get(`/api/dps_group/${v}`)    
+function getSchoolRank(v) {
+    return $helper().get(`/api/dps_group/${v}`);
 }
 
-export {getSchoolRank}
+function getItemPrice(params) {
+    return $helper().get("/api/item_groups/with_price", {
+        params: params,
+    });
+}
+
+export { getSchoolRank, getItemPrice };
