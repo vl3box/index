0972bde71a3c59333d7a760cd80355c0c856bb5a
iRuxu <rx6@qq.com>
2021-07-28 15:56:51 +0800

    update box

--- src/store/index.js
+++ src/store/index.js
@@ -5,23 +5,21 @@ Vue.use(Vuex);
 
 let store = {
     state: {
-        profile : {
-            server : '蝶恋花'
+        profile: {
+            server: "蝶恋花",
         },
-        server : '蝶恋花',
-        config : {
-            index_live_status : 0,
-            index_video_status : 0,
-            index_live_url : '',
-            index_video_url : ''
-        }
+        server: "蝶恋花",
+        config: {
+            index_live_status: 0,
+            index_video_status: 0,
+            index_live_url: "",
+            index_video_url: "",
+        },
     },
-    mutations: {
-    },
-    getters: {
-    },
+    mutations: {},
+    getters: {},
     actions: {},
-    modules: {}
+    modules: {},
 };
 
 export default new Vuex.Store(store);
