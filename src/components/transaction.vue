2f4a4a2aa323cc205aec88486025b5eb6ead0e4c
iRuxu <rx6@qq.com>
2021-09-14 14:11:00 +0800

    sync common

--- src/components/transaction.vue
+++ src/components/transaction.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="m-index-transaction m-sideblock">
         <div class="m-sideblock-header">
-            <i class="el-icon-coin"></i>
-            <span class="u-title">全服交易行</span>
+            <span class="u-title"><i class="el-icon-coin"></i> 全服交易行</span>
             <el-select class="u-server" v-model="server" placeholder="请选择服务器" size="mini">
                 <el-option v-for="serve in servers" :key="serve" :label="serve" :value="serve"></el-option>
             </el-select>
+            <el-input class="u-search" placeholder="请输入内容" v-model="search" size="mini" @keyup.enter.native="goItemPage">
+                <el-button slot="append" icon="el-icon-search" @click="goItemPage"></el-button>
+            </el-input>
             <a href="/item" class="u-more" target="_blank" rel="noopener noreferrer" title="查看全部">
                 <i class="el-icon-more"></i>
             </a>
         </div>
         <div class="m-transaction-box">
             <div class="m-price-list" v-if="groups && groups.length">
-                <el-carousel indicator-position="none" :autoplay="true" :interval="3000" height="200px" direction="vertical">
-                    <el-carousel-item  v-for="(group, key) in groups" :key="key">
+                <el-carousel
+                    indicator-position="none"
+                    :autoplay="true"
+                    :interval="3000"
+                    height="200px"
+                    direction="vertical"
+                >
+                    <el-carousel-item v-for="(group, key) in groups" :key="key">
                         <el-row :gutter="20">
                             <!-- <div class="u-group-title" v-text="group.label"></div> -->
                             <el-col :span="6" v-for="(item, k) in group.items" :key="k">
@@ -32,7 +40,10 @@
                                             <span v-text="item.label"></span>
                                         </span>
                                         <span class="u-price">
-                                            <span class="u-trending" :class="item | showItemTrendingClass">{{item | showItemTrending}}</span>
+                                            <span
+                                                class="u-trending"
+                                                :class="item | showItemTrendingClass"
+                                            >{{item | showItemTrending}}</span>
                                             <template v-if="item.sub_days_0_price">
                                                 <!-- <span>今日：</span> -->
                                                 <GamePrice :price="item.sub_days_0_price" />
@@ -63,11 +74,11 @@
 </template>
 
 <script>
-import servers from "@jx3box/jx3box-data/data/server/server_cn.json";
+import servers from "@jx3box/jx3box-data/data/server/server_std.json";
 import GamePrice from "./GamePrice.vue";
 import User from "@jx3box/jx3box-common/js/user";
 import { getItemPrice } from "@/service/helper.js";
-import {iconLink} from '@jx3box/jx3box-common/js/utils'
+import { iconLink } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "transaction",
     props: [],
@@ -79,23 +90,24 @@ export default {
             groups: [],
             server: "蝶恋花",
             servers: servers,
+            search : ''
         };
     },
     methods: {
         loadData: function () {
             getItemPrice({
                 server: this.server,
-                keys: [
-                    "index1",
-                    "index2",
-                    "teshucailiao"
-                ],
+                keys: ["index1", "index2", "teshucailiao"],
             }).then((data) => {
                 data = data.data;
                 if (data.code === 200)
                     this.groups = Object.values(data.data.data) || [];
             });
         },
+        goItemPage : function (){
+            let host = location.origin
+            window.open(`${host}/item/#/search/${this.search}?page=1`,'_blank')
+        }
     },
     watch: {
         server: {
@@ -107,37 +119,37 @@ export default {
     },
     mounted: function () {
         if (User.isLogin()) {
-            this.server = this.$store.state.server || '蝶恋花';
+            this.server = this.$store.state.server || "蝶恋花";
         }
     },
-    filters : {
+    filters: {
         iconLink,
-        showItemLink : function (val){
-            return `/item/#/view/${val}`
+        showItemLink: function (val) {
+            return `/item/#/view/${val}`;
         },
-        showItemTrending : function (item){
-            if(item.sub_days_0_price && item.sub_days_1_price){
-                if((item.sub_days_0_price - item.sub_days_1_price) > 0){
-                    return '▲'
-                }else if((item.sub_days_0_price - item.sub_days_1_price) < 0){
-                    return '▼'
-                }else{
-                    return ''
+        showItemTrending: function (item) {
+            if (item.sub_days_0_price && item.sub_days_1_price) {
+                if (item.sub_days_0_price - item.sub_days_1_price > 0) {
+                    return "▲";
+                } else if (item.sub_days_0_price - item.sub_days_1_price < 0) {
+                    return "▼";
+                } else {
+                    return "";
                 }
             }
         },
-        showItemTrendingClass : function (item){
-            if(item.sub_days_0_price && item.sub_days_1_price){
-                if((item.sub_days_0_price - item.sub_days_1_price) > 0){
-                    return 'up'
-                }else if((item.sub_days_0_price - item.sub_days_1_price) < 0){
-                    return 'down'
-                }else{
-                    return 'keep'
+        showItemTrendingClass: function (item) {
+            if (item.sub_days_0_price && item.sub_days_1_price) {
+                if (item.sub_days_0_price - item.sub_days_1_price > 0) {
+                    return "up";
+                } else if (item.sub_days_0_price - item.sub_days_1_price < 0) {
+                    return "down";
+                } else {
+                    return "keep";
                 }
             }
-        }
-    }
+        },
+    },
 };
 </script>
 
