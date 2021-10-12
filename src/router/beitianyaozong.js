31b59dbe941a3bf478c738c3b2849a407b1fff5c
Kaviilee <jiaweilee95@126.com>
2021-10-13 00:14:34 +0800

    fix: multiple key

--- src/router/beitianyaozong.js
+++ src/router/beitianyaozong.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-const Index = () => import("../topic/beitianyaozong/Index.vue");
+const Index = () => import("../topic/beitianyaozong/index.vue");
 const Yaozong = () => import("../topic/beitianyaozong/Yaozong.vue");
 const Fb = () => import("../topic/beitianyaozong/Fb.vue");
 
