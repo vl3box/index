839e0a485ab6a642867323ec862fd04b9bbabf85
iRuxu <rx6@qq.com>
2021-10-15 23:45:34 +0800

    update

--- src/topic/beitianyaozong/equip.vue
+++ src/topic/beitianyaozong/equip.vue
@@ -1,45 +1,42 @@
 <template>
-  <div class="m-yaozong-equip">
-    <span class="u-next" @click="turn"></span>
-    <img
-      :src="img"
-      :class="[`img` + index, show ? 'p-animations bounceIn' : '']"
-    />
-  </div>
+    <div class="m-yaozong-equip">
+        <span class="u-next" @click="turn"></span>
+        <img :src="img" :class="[`img` + index, show ? 'p-animations fadeIn' : '']" />
+    </div>
 </template>
 
 <script>
 export default {
-  name: "equip",
-  props: ["img_root"],
-  components: {},
-  data: function () {
-    return {
-      index: 1,
-      len: 4,
-      show: true,
-    };
-  },
-  computed: {
-    img: function () {
-      return this.img_root + `yh` + this.index + `.png`;
+    name: "equip",
+    props: ["img_root"],
+    components: {},
+    data: function () {
+        return {
+            index: 1,
+            len: 4,
+            show: true,
+        };
     },
-  },
-  watch: {},
-  methods: {
-    turn: function () {
-      this.show = false;
-      setTimeout(() => {
-        this.show = true;
-      }, 10);
-      this.index++;
-      if (this.index > this.len) {
-        this.index = 1;
-      }
+    computed: {
+        img: function () {
+            return this.img_root + `yh` + this.index + `.png`;
+        },
     },
-  },
-  filters: {},
-  created: function () {},
-  mounted: function () {},
+    watch: {},
+    methods: {
+        turn: function () {
+            this.show = false;
+            setTimeout(() => {
+                this.show = true;
+            }, 10);
+            this.index++;
+            if (this.index > this.len) {
+                this.index = 1;
+            }
+        },
+    },
+    filters: {},
+    created: function () {},
+    mounted: function () {},
 };
 </script>
--- src/topic/beitianyaozong/skill.vue
+++ src/topic/beitianyaozong/skill.vue
@@ -1,72 +1,72 @@
 <template>
-  <div class="m-yaozong-skill">
-    <div class="u-tab">
-      <img :src="img_root + `zhaoshi.png`" class="u-logo" />
-      <span
-        v-for="(item, i) in tab"
-        :key="item"
-        :class="[item, i == index ? 'active' : '']"
-        @click="changeTab(i)"
-      ></span>
+    <div class="m-yaozong-skill">
+        <div class="u-tab">
+            <img :src="img_root + `zhaoshi.png`" class="u-logo" />
+            <span
+                v-for="(item, i) in tab"
+                :key="item"
+                :class="[item, i == index ? 'active' : '']"
+                @click="changeTab(i)"
+            ></span>
+        </div>
+        <div class="u-cont">
+            <span class="u-prev" @click="turn()" v-show="contIndex > 1"></span>
+            <span class="u-next" @click="turn(true)" v-show="contIndex < len"></span>
+            <img :src="contImg" :class="animation ? 'p-animation fadeIn' : ''" />
+        </div>
     </div>
-    <div class="u-cont">
-      <span class="u-prev" @click="turn()" v-show="contIndex > 1"></span>
-      <span class="u-next" @click="turn(true)" v-show="contIndex < len"></span>
-      <img :src="contImg" :class="animation ? 'p-animation bounceIn' : ''" />
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "skill",
-  props: ["img_root"],
-  components: {},
-  data: function () {
-    return {
-      animation: true,
-      index: 0,
-      contIndex: 1,
-      tab: ["lingsu", "tongyong", "wufang"],
-    };
-  },
-  computed: {
-    contImg() {
-      return (
-        this.img_root +
-        "/" +
-        this.tab[this.index] +
-        "/" +
-        this.contIndex +
-        ".png"
-      );
+    name: "skill",
+    props: ["img_root"],
+    components: {},
+    data: function () {
+        return {
+            animation: true,
+            index: 0,
+            contIndex: 1,
+            tab: ["lingsu", "tongyong", "wufang"],
+        };
     },
-    len() {
-      if (this.index == 1) return 5;
-      if (this.index == 2) return 8;
-      return 7;
+    computed: {
+        contImg() {
+            return (
+                this.img_root +
+                "/" +
+                this.tab[this.index] +
+                "/" +
+                this.contIndex +
+                ".png"
+            );
+        },
+        len() {
+            if (this.index == 1) return 5;
+            if (this.index == 2) return 8;
+            return 7;
+        },
     },
-  },
-  watch: {},
-  methods: {
-    changeTab(i) {
-      this.index = i;
-      this.contIndex = 1;
+    watch: {},
+    methods: {
+        changeTab(i) {
+            this.index = i;
+            this.contIndex = 1;
+        },
+        turn: function (direction) {
+            this.animation = false;
+            setTimeout(() => {
+                this.animation = true;
+            }, 10);
+            if (direction) {
+                this.contIndex++;
+            } else {
+                this.contIndex--;
+            }
+        },
     },
-    turn: function (direction) {
-      this.animation = false;
-      setTimeout(() => {
-        this.animation = true;
-      }, 10);
-      if (direction) {
-        this.contIndex++;
-      } else {
-        this.contIndex--;
-      }
-    },
-  },
-  filters: {},
-  created: function () {},
-  mounted: function () {},
+    filters: {},
+    created: function () {},
+    mounted: function () {},
 };
 </script>
