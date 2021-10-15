dd62817851556da0d1e009a373283bc3e17e5b77
iRuxu <rx6@qq.com>
2021-10-15 18:01:32 +0800

    opt topic beitianyaozong

--- src/topic/beitianyaozong/Yaozong.vue
+++ src/topic/beitianyaozong/Yaozong.vue
@@ -1,50 +1,43 @@
 <template>
-  <div class="p-topic-yaozong-wrapper">
-    <div
-      class="p-yaozong"
-      :style="{ backgroundImage: changebg ? otherImg : getBgUrl(index) }"
-    >
-      <div class="m-yaozong-bg">
-        <img
-          :src="img_root + 'bg' + i + '.jpg'"
-          v-for="(item, i) in 6"
-          :key="i"
-        />
-      </div>
-      <div class="wp p-yaozong-container">
-        <div class="m-yaozong-box">
-          <!-- 左侧菜单 -->
-          <div class="m-yaozong-menu">
-            <div class="u-menubg"></div>
-            <div
-              v-for="(item, i) in menu"
-              :key="item"
-              :class="i == index ? 'active' : ''"
-              @click="changeMenu(i)"
-              class="u-menubox"
-            >
-              <span :class="`u-menu` + (i + 1)"></span>
+    <div class="p-topic-yaozong-wrapper">
+        <div class="p-yaozong" :style="{ backgroundImage: changebg ? otherImg : getBgUrl(index) }">
+            <div class="m-yaozong-bg">
+                <img :src="img_root + 'bg' + i + '.jpg'" v-for="(item, i) in 6" :key="i" />
             </div>
-          </div>
-          <!-- 右侧内容 -->
-          <div class="m-yaozong-content">
-            <component
-              :is="focus"
-              :img_root="img_root"
-              :index="index"
-              @changeBg="changeBg"
-            />
-          </div>
+            <div class="wp p-yaozong-container">
+                <div class="m-yaozong-box">
+                    <!-- 左侧菜单 -->
+                    <div class="m-yaozong-menu">
+                        <div class="u-menubg"></div>
+                        <div
+                            v-for="(item, i) in menu"
+                            :key="item"
+                            :class="i == index ? 'active' : ''"
+                            @click="changeMenu(i)"
+                            class="u-menubox"
+                        >
+                            <span :class="`u-menu` + (i + 1)"></span>
+                        </div>
+                    </div>
+                    <!-- 右侧内容 -->
+                    <div class="m-yaozong-content">
+                        <component
+                            :is="focus"
+                            :img_root="img_root"
+                            :index="index"
+                            @changeBg="changeBg"
+                        />
+                    </div>
+                </div>
+                <!-- 山水云 -->
+                <div class="m-yaozong-landscape">
+                    <img class="lb" :src="img_root + 'land-lb.png'" />
+                    <img class="rt" :src="img_root + 'land-rt.png'" />
+                    <img class="rb" :src="img_root + 'land-rb.png'" />
+                </div>
+            </div>
         </div>
-        <!-- 山水云 -->
-        <div class="m-yaozong-landscape">
-          <img class="lb" :src="img_root + 'land-lb.png'" />
-          <img class="rt" :src="img_root + 'land-rt.png'" />
-          <img class="rb" :src="img_root + 'land-rb.png'" />
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -57,70 +50,70 @@ import equip from "./equip.vue";
 import story from "./story.vue";
 
 export default {
-  name: "Yaozong",
-  props: [],
-  components: {},
-  data: function () {
-    return {
-      // 背景ROOT
-      img_root: __imgPath + "topic/beitianyaozong/sect/",
-      // 当前索引
-      index: 0,
-      menu: ["story", "mount", "skill", "weapon", "equip", "scenery"],
-      changebg: false,
-      otherImg: "",
-    };
-  },
-  computed: {
-    focus: function () {
-      return this.menu[this.index];
+    name: "Yaozong",
+    props: [],
+    components: {},
+    data: function () {
+        return {
+            // 背景ROOT
+            img_root: __imgPath + "topic/beitianyaozong/sect/",
+            // 当前索引
+            index: 0,
+            menu: ["story", "mount", "skill", "weapon", "equip", "scenery"],
+            changebg: false,
+            otherImg: "",
+        };
     },
-  },
-  watch: {},
-  methods: {
-    changeMenu(i) {
-      this.index = i;
-      this.changebg = false;
+    computed: {
+        focus: function () {
+            return this.menu[this.index];
+        },
     },
-    getBgUrl: function (i) {
-      return `url('${this.img_root}bg${i}.jpg')`;
+    watch: {},
+    methods: {
+        changeMenu(i) {
+            this.index = i;
+            this.changebg = false;
+        },
+        getBgUrl: function (i) {
+            return `url('${this.img_root}bg${i}.jpg')`;
+        },
+        getWeaponUrl: function (i) {
+            if (i == 3) {
+                return `url('${this.img_root}bg${i}.jpg')`;
+            }
+            return `url('${this.img_root}wuqi${i}.jpg')`;
+        },
+        getEquipUrl: function (i) {
+            if (i == 5) {
+                return `url('${this.img_root}bg${i}.jpg')`;
+            }
+            return `url('${this.img_root}scenery${i}.jpg')`;
+        },
+        changeBg(i, name) {
+            this.changebg = true;
+            if (name == "weapon") {
+                this.otherImg = this.getWeaponUrl(i);
+            } else {
+                this.otherImg = this.getEquipUrl(i);
+            }
+        },
     },
-    getWeaponUrl: function (i) {
-      if (i == 3) {
-        return `url('${this.img_root}bg${i}.jpg')`;
-      }
-      return `url('${this.img_root}wuqi${i}.jpg')`;
+    filters: {},
+    created: function () {},
+    mounted: function () {},
+    components: {
+        mount,
+        scenery,
+        skill,
+        weapon,
+        equip,
+        story,
     },
-    getEquipUrl: function (i) {
-      if (i == 5) {
-        return `url('${this.img_root}bg${i}.jpg')`;
-      }
-      return `url('${this.img_root}scenery${i}.jpg')`;
-    },
-    changeBg(i, name) {
-      this.changebg = true;
-      if (name == "weapon") {
-        this.otherImg = this.getWeaponUrl(i);
-      } else {
-        this.otherImg = this.getEquipUrl(i);
-      }
-    },
-  },
-  filters: {},
-  created: function () {},
-  mounted: function () {},
-  components: {
-    mount,
-    scenery,
-    skill,
-    weapon,
-    equip,
-    story,
-  },
 };
 </script>
  
- <style lang="less">
-@import "../../assets/css/topic/beitianyaozongSect.less";
+<style lang="less">
+@import "../../assets/css/topic/beitianyaozong/beitianyaozongSect.less";
 </style>
  
