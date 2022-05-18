dbe7b020546759b0c2592d4cc39a362a46f7d21b
Kaviilee <jiaweilee95@126.com>
2022-05-18 15:36:50 +0800

    feat: v2 calendar 日期点击事件

--- src/components/v2/calendar.vue
+++ src/components/v2/calendar.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="m-calendar">
         <div class="m-calendar-content">
-            <div class="m-calendar-item" :class="{ current: isToday(item) }" v-for="(item, index) in data" :key="index">
+            <a
+                class="m-calendar-item"
+                :class="{ current: isToday(item) }"
+                v-for="(item, index) in data"
+                :key="index"
+                :href="`/calendar/archive/${item.year}/${item.month}/${item.date}`"
+                target="_blank"
+            >
                 <div class="u-week">{{ item.week }}</div>
                 <div class="u-date">{{ item.date }}</div>
-            </div>
+            </a>
         </div>
         <div class="m-calendar-event">
             <template v-for="(item, index) in list">
@@ -16,10 +23,10 @@
 
 <script>
 import calendar_item from "./calendar_item.vue";
-import { getRangeCalendar } from '@/service/cms.js';
-import dayjs from 'dayjs';
+import { getRangeCalendar } from "@/service/cms.js";
+import dayjs from "dayjs";
 export default {
-    name: 'calendar',
+    name: "calendar",
     components: {
         "calendar-item": calendar_item,
     },
@@ -29,18 +36,18 @@ export default {
             list: [],
             weeks: ["日", "一", "二", "三", "四", "五", "六"],
             current: {
-                year: '',
-                month: '',
-                date: '',
+                year: "",
+                month: "",
+                date: "",
             },
-        }
+        };
     },
     computed: {
         dateRange() {
             let dataRange = [];
 
             for (let i = 0; i < 7; i++) {
-                const date = dayjs().add(i, 'day').format('YYYY-MM-DD');
+                const date = dayjs().add(i, "day").format("YYYY-MM-DD");
                 dataRange.push(date);
             }
 
@@ -48,10 +55,10 @@ export default {
         },
         params() {
             return {
-                dateRange: this.dateRange.join(','),
-                client: this.$store.state.client
-            }
-        }
+                dateRange: this.dateRange.join(","),
+                client: this.$store.state.client,
+            };
+        },
     },
     watch: {
         dateRange: {
@@ -59,8 +66,8 @@ export default {
             immediate: true,
             handler(val) {
                 this.initCalendar();
-            }
-        }
+            },
+        },
     },
     mounted() {
         const today = new Date();
@@ -72,34 +79,36 @@ export default {
     },
     methods: {
         initCalendar() {
-            this.dateRange.forEach(date => {
-                const year = ~~date.split('-')[0];
-                const month = ~~date.split('-')[1];
-                const day = ~~date.split('-')[2];
+            this.dateRange.forEach((date) => {
+                const year = ~~date.split("-")[0];
+                const month = ~~date.split("-")[1];
+                const day = ~~date.split("-")[2];
                 this.data.push({
                     year,
                     month,
                     date: day,
                     isToday: this.isToday({ year, month, date: day }),
-                    week: '周' + this.weeks[new Date(year, month - 1, day).getDay()],
-                    children: []
-                })
-            })
+                    week: "周" + this.weeks[new Date(year, month - 1, day).getDay()],
+                    children: [],
+                });
+            });
 
             this.loadRangeCalendar();
         },
         loadRangeCalendar() {
-            getRangeCalendar(this.params).then(res => {
+            getRangeCalendar(this.params).then((res) => {
                 const { data } = res.data;
-                this.list = data || []
+                this.list = data || [];
                 if (data) {
-                    data?.forEach(item => {
-                        const index = this.data.findIndex(v => v.year === item.year && v.month === item.month && v.date === item.date);
+                    data?.forEach((item) => {
+                        const index = this.data.findIndex(
+                            (v) => v.year === item.year && v.month === item.month && v.date === item.date
+                        );
 
                         index && this.data[index].children.push(item);
-                    })
+                    });
                 }
-            })
+            });
         },
         // 判断是否为今日
         isToday({ year, month, date }) {
@@ -107,8 +116,8 @@ export default {
 
             return dateObj.getFullYear() === year && dateObj.getMonth() + 1 === month && dateObj.getDate() === date;
         },
-    }
-}
+    },
+};
 </script>
 
 <style lang="less" scoped>
@@ -139,7 +148,6 @@ export default {
                 .mb(5px);
             }
         }
-
     }
     &-event {
         .mt(10px);
