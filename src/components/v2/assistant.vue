3333f648ce8573d2529b242ee8b0f05a8d17082a
iRuxu <rx6@qq.com>
2022-10-16 04:34:55 +0800

    fix:ass

--- src/components/v2/assistant.vue
+++ src/components/v2/assistant.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="m-assistant" :style="{ backgroundImage: bg }">
         <div class="u-assistant">
-            <a :href="link" target="_blank" class="u-btn button button-3d button-primary button-rounded"><i class="el-icon-download"></i>魔盒助手</a>
-            <span class="u-label">{{ label }}</span>
+            <a href="/tool/39410" target="_blank" class="u-btn button button-3d button-primary button-rounded"
+                ><i class="el-icon-download"></i>魔盒助手</a
+            >
+            <span class="u-label" v-html="data"></span>
         </div>
     </div>
 </template>
@@ -10,7 +12,8 @@
 <script>
 import { theme } from "../../../setting.json";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
-import { getMenus, getEventV2 } from "@/service/cms.js";
+import { getEventV2 } from "@/service/cms.js";
+import { getBreadcrumb } from "@jx3box/jx3box-common/js/api_misc";
 import { resolveImagePath } from "@jx3box/jx3box-common/js/utils";
 export default {
     name: "assistant",
@@ -18,7 +21,7 @@ export default {
         return {
             label: "",
             link: "",
-            bg: ''
+            bg: "",
         };
     },
     computed: {
@@ -27,26 +30,24 @@ export default {
         },
         theme: function () {
             return theme[this.client];
-        }
+        },
     },
 
     methods: {
         loadData: function () {
-            return getMenus("assistant").then((res) => {
-                let data = res.data.data.val?.[0];
-                this.label = data.label;
-                this.link = data.link;
+            return getBreadcrumb("index_jba").then((data) => {
+                this.data = data;
             });
         },
         loadBackGround: function () {
             getEventV2({
-                type: 'common',
-                subtype: 'jba',
+                type: "common",
+                subtype: "jba",
                 status: 1,
-                client: this.client
+                client: this.client,
             }).then((res) => {
-                const url = resolveImagePath(res.data.data.list[0]?.img)
-                if (url) this.bg = `url(${url})`
+                const url = resolveImagePath(res.data.data.list[0]?.img);
+                if (url) this.bg = `url(${url})`;
             });
         },
     },
