e7b98fb5b2ce5c35e41523f6915fd2f56086bc86
litthy <litthy@qq.com>
2022-11-21 15:38:10 +0800

    fix: face

--- src/components/index/face.vue
+++ src/components/index/face.vue
@@ -22,13 +22,9 @@
 </template>
 
 <script>
-import {
-    buildTarget,
-    showBanner,
-    getLink,
-} from "@jx3box/jx3box-common/js/utils";
+import { buildTarget, getThumbnail, getLink } from "@jx3box/jx3box-common/js/utils";
 import { getFaceList } from "@/service/index.js";
-import {__imgPath} from '@jx3box/jx3box-common/data/jx3box.json'
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 export default {
     name: "face",
     props: [],
@@ -43,24 +39,24 @@ export default {
         target: function () {
             return buildTarget();
         },
-        per : function (){
-            return 9
-            let w = window.innerWidth
-            let space = w
-            let count = 10
-            if(w<1280){
-                space = w - 30*2
-            }else if(w<=1680){
-                space = w - 30*2 - 350
-            }else{
-                space = w - 30*2 - 350 - 320
+        per: function () {
+            return 9;
+            let w = window.innerWidth;
+            let space = w;
+            let count = 10;
+            if (w < 1280) {
+                space = w - 30 * 2;
+            } else if (w <= 1680) {
+                space = w - 30 * 2 - 350;
+            } else {
+                space = w - 30 * 2 - 350 - 320;
             }
-            count = Math.floor(space / 150) * 2
+            count = Math.floor(space / 150) * 2;
 
-            if(w<768){
-                count = 9
+            if (w < 768) {
+                count = 9;
             }
-            return count
+            return count;
         },
         client: function () {
             return this.$store.state.client;
@@ -68,9 +64,9 @@ export default {
     },
     methods: {
         showThumb: function (item) {
-            let url = item.images[0]
+            let url = item.images[0];
             if (url) {
-                return showBanner(url, "face");
+                return getThumbnail(url);
             } else {
                 return __imgPath + "image/face/null2.png";
             }
