9c8fbda6af126b554b99d4a9b51002ed073a34f2
qaz528093426 <528093426@qq.com>
2022-12-23 18:51:24 +0800

    feat：issues-45 (圣诞贺卡) -圣诞贺卡样式修改

--- src/components/festival/christmas.vue
+++ src/components/festival/christmas.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="christmas" @click.stop>
-        <div class="christmas-card">
+        <div class="christmas-card" @click="change">
             <img v-if="!show" class="christmas-card__img" :src="`${imgLink}/christmas/cover.jpg`" alt="圣诞节快乐">
-            <img v-if="!show" @click="change" class="christmas-card__open" :src="`${imgLink}/christmas/button.png`"
-                alt="圣诞节快乐">
-            <img v-if="show" @click="close" class="animation christmas-card__content christmas-card__img"
+            <img v-if="!show" class="christmas-card__open" :src="`${imgLink}/christmas/button.png`" alt="圣诞节快乐">
+            <img v-if="show" @click.stop="close" class="animation christmas-card__content christmas-card__img"
                 :class="show? 'flipInY' : ''" :src="`${imgLink}/christmas/${fontCount || 0}.png`" alt="圣诞节快乐">
         </div>
 
