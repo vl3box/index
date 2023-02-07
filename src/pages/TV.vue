76e48bad7c3b0944eb5086fb12bba437b9b8b7e3
litthy <litthy@qq.com>
2023-02-07 21:38:13 +0800

    feat: tv

--- src/pages/TV.vue
+++ src/pages/TV.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="p-tv"></div>
+    <div class="p-tv">
+        <Header :overlayEnable="true"></Header>
+        <div class="m-tv-main">
+            <div class="m-tv-content">
+                <img src="../assets/img/tv/box.png" alt="盒子娘" class="m-jx3box">
+            </div>
+        </div>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
@@ -24,5 +32,5 @@ export default {
 </script>
 
 <style lang="less">
-// @import "../assets/css/kv.less";
+@import "~@/assets/css/tv/index.less";
 </style>
