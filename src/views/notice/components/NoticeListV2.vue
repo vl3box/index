e613e05a49d632c956e1b89a64da0ada7063e611
zixuan1986 <litthy@qq.com>
2023-02-17 10:56:39 +0800

    fix: search

--- src/views/notice/components/NoticeListV2.vue
+++ src/views/notice/components/NoticeListV2.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="m-notice-content">
-        <!-- 功能条 -->
-        <NoticeToolbar @update="update" />
+        <!-- 筛选 & 搜索 -->
+        <div class="m-notice-toolbar">
+            <div class="m-buttons">
+                <span
+                    class="u-button"
+                    v-for="(item, i) in buttons"
+                    :key="i"
+                    @click="change(item.value)"
+                    :class="[{ active: subtype == item.value || subtype == 0 }, item.key]"
+                >
+                    {{ item.name }}
+                </span>
+            </div>
+            <el-input class="u-search" v-model.lazy="search" placeholder="请输入关键词搜索" clearable></el-input>
+        </div>
         <!-- 列表 -->
         <div class="m-notice-box">
             <div class="m-notice-list" v-loading="loading">
@@ -9,10 +22,10 @@
                     <img
                         class="u-icon"
                         svg-inline
-                        src="../../../assets/img/notice/post.svg"
-                        v-if="item.post_subtype == 2"
+                        src="../../../assets/img/notice/up.svg"
+                        v-if="item.post_subtype == 3"
                     />
-                    <img class="u-icon" svg-inline src="../../../assets/img/notice/up.svg" v-else />
+                    <img class="u-icon" svg-inline src="../../../assets/img/notice/post.svg" v-else />
                     <span class="u-title"> {{ item.post_title }}</span>
                 </div>
             </div>
@@ -44,11 +57,8 @@
 </template>
 <script>
 import { getPosts } from "@/service/cms";
-import notice_types from "@/assets/data/notice_types.json";
-import NoticeToolbar from "./NoticeToolbar.vue";
 export default {
     name: "NoticeList",
-    components: { NoticeToolbar },
     data: function () {
         return {
             loading: false, //加载状态
@@ -60,14 +70,42 @@ export default {
             per: 10, //每页条目
             index: "", // 跳转页面
 
-            search: "",
             client: this.$store.state.client, //版本选择
-            isAdmin: false,
-            notice_types,
-            subtype: "",
+            search: "",
+            subtype: 0,
+
+            buttons: [
+                // {
+                //     name: "游戏资讯",
+                //     key: "information",
+                //     value: 1,
+                // },
+                {
+                    name: "魔盒公告",
+                    key: "notice",
+                    value: 2,
+                },
+                {
+                    name: "功能更新",
+                    key: "update",
+                    value: 3,
+                },
+                {
+                    name: "兑换通知",
+                    key: "message",
+                    value: 4,
+                },
+            ],
         };
     },
     computed: {
+        query() {
+            return {
+                client: this.client,
+                search: this.search,
+                subtype: this.subtype,
+            };
+        },
         params: function () {
             let params = {
                 per: this.per,
@@ -75,11 +113,9 @@ export default {
                 sticky: 1,
                 type: "notice",
             };
-            let optionalParams = ["search", "client", "subtype"];
-            optionalParams.forEach((item) => {
-                if (this[item]) {
-                    params[item] = this[item];
-                }
+            const optionalParams = this.query;
+            Object.keys(optionalParams).forEach((item) => {
+                if (this[item]) params[item] = this[item];
             });
             return params;
         },
@@ -112,11 +148,9 @@ export default {
             this.changePage(this.index);
             this.index = "";
         },
-        // 搜索
-        update({ search, subtype }) {
-            this.search = search;
-            this.subtype = subtype;
-            this.page = 1;
+        // 切换类型
+        change(value) {
+            this.subtype = value;
         },
     },
 
@@ -128,107 +162,12 @@ export default {
                 this.loadPosts();
             },
         },
+        query() {
+            this.page = 1;
+        },
     },
 };
 </script>
 <style lang="less">
-.m-notice-list {
-    .flex;
-    flex-direction: column;
-    gap: 27px;
-    .m-list {
-        .pointer;
-        .color(#282c31);
-        .size(100%,43px);
-        .lh(43px);
-        .r(20px);
-        position: relative;
-        box-sizing: border-box;
-        padding: 0 68px;
-        letter-spacing: 1px;
-        background-color: #fff;
-        box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.1);
-        transition: all 0.5s ease-in-out;
-        &:hover {
-            color: #fff;
-            background-color: #282c31;
-            .u-icon path {
-                fill: #fff;
-            }
-        }
-        .u-title {
-            .db;
-            .break(1);
-        }
-        .u-icon {
-            .pa;
-            .lt(10px, -2px);
-            path {
-                fill: #282c31;
-            }
-        }
-    }
-}
-// 分页
-.p-notice-pagination {
-    .flex;
-    .pt(20px);
-    gap: 10px;
-    justify-content: flex-end;
-    .el-pager {
-        .flex;
-        gap: 10px;
-        li {
-            .size(42px);
-            .r(100%);
-            .lh(42px);
-            background-color: #fff;
-            color: #282c31;
-            box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.1);
-            &:hover,
-            &.active {
-                background-color: #282c31;
-                color: #fff;
-            }
-        }
-    }
-    // 跳转
-    .m-jump {
-        .flex;
-        .size(auto,42px);
-        .r(20px);
-        .lh(42px);
-        align-items: center;
-        padding: 0 12px;
-        gap: 10px;
-        background-color: #fff;
-        box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.1);
-        .u-input {
-            .w(98px);
-        }
-        .u-jump {
-            .r(20px);
-            .fz(14px,28px);
-            .h(28px);
-            .pointer;
-            .bold;
-            color: #fff;
-            padding: 0 12px;
-            background-color: #282c31;
-        }
-        .el-input__inner {
-            .r(20px);
-            box-sizing: border-box;
-            box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.1);
-        }
-    }
-}
-.m-notice-content {
-    .flex;
-    box-sizing: border-box;
-    min-height: calc(100vh - @header-height + 16px);
-    padding: 84px + @header-height 0 30px 0;
-    flex-direction: column;
-    gap: 24px;
-}
+@import "~@/assets/css/notice/list-v2.less";
 </style>
