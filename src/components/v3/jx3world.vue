7334f2c5f15b6e2b24cefbf4b514d6602041869f
iRuxu <rx6@qq.com>
2023-05-22 03:59:05 +0800

    perf:left sidebar

--- src/components/v3/jx3world.vue
+++ src/components/v3/jx3world.vue
@@ -10,7 +10,7 @@
                     </a>
                 </el-tooltip> -->
                 <i class="u-icon el-icon-s-flag"></i>
-                <span class="u-title">江湖快报</span>
+                <a class="u-title" href="/pvg/gonggao/daily" target="_blank">江湖快报</a>
                 <!-- <div class="u-mode-list">
                     <span
                         :class="`u-mode u-mode-${mode} ${item.value == mode ? 'is-active' : ''}`"
