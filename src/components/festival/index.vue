8815ae3cbe951287c4a9f95d79f0f9b6ba4ec0b3
litthy <litthy@qq.com>
2023-09-06 11:11:38 +0800

    add: midAutumn

--- src/components/festival/index.vue
+++ src/components/festival/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-index-popup" v-if="success" v-show="visible" @click="close">
         <!-- 贺卡 -->
-        <sevenFestival :fontCount="count" @close="closePop" @checked="checked" />
+        <midAutumn :fontCount="count" @checked="checked" @close="close" />
     </div>
 </template>
 
@@ -9,7 +9,7 @@
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 import { getBoxCoin } from "@/service/index";
 import User from "@jx3box/jx3box-common/js/user";
-import sevenFestival from "@/components/festival/sevenFestival.vue";
+import midAutumn from "@/components/festival/midAutumn.vue";
 export default {
     name: "Festival",
     data: function () {
@@ -22,7 +22,7 @@ export default {
             check: false,
         };
     },
-    components: { sevenFestival },
+    components: { midAutumn },
     computed: {
         imgLink: function ({ event_id }) {
             return __imgPath + `topic/festival/`;
---
+++ src/components/festival/midAutumn.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="midAutumn">
+        <img :src="`${imgLink}202302.png`" class="u-img" @click="close" />
+        <img :src="`${imgLink}202301.png`" class="u-img-cover" :class="{ active }" @click.stop="hide" />
+    </div>
+</template>
+<script>
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+export default {
+    name: "midAutumn",
+    props: ["fontCount"],
+    computed: {
+        imgLink() {
+            return __imgPath + `topic/festival/midAutumn/`;
+        },
+    },
+    data() {
+        return {
+            active: false,
+        };
+    },
+    methods: {
+        close() {
+            this.active = false;
+            this.$emit("checked");
+            this.$emit("close");
+        },
+        hide() {
+            this.active = true;
+            this.$emit("checked");
+        },
+    },
+};
+</script>
+<style lang="less">
+.midAutumn {
+    .pr;
+    .pointer;
+    .size(600px,auto);
+    .auto(x);
+    .mt(80px);
+
+    @media screen and(max-width:@ipad) {
+        .w(500px);
+    }
+    @media screen and(max-width:@phone) {
+        .w(80%);
+    }
+    @media screen and (max-height: 820px) and (max-width: 1280px) {
+        .w(360px);
+    }
+    @media screen and (max-height: 820px) and (max-width: @phone) {
+        .w(80%);
+        max-width: 320px;
+    }
+    .u-img-cover {
+        .pa;
+        .lt(0);
+        transition-duration: 0.8s;
+        &:hover {
+            top: -20px;
+        }
+        &.active {
+            .none;
+        }
+    }
+}
+</style>
