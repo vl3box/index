b2324d3a2b5cc5cd17ba29b72af28a98e958fcd7
fifththirteen <570205219@qq.com>
2023-11-06 18:30:58 +0800

    fix:名望

--- src/components/v4/world/celebrity.vue
+++ src/components/v4/world/celebrity.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="m-celebrity-wrap">
-        <el-divider content-position="left">名望 · 楚天社</el-divider>
+        <el-divider content-position="left">
+            名望&nbsp;·&nbsp;
+            <el-select v-model="type" @change="typeChange">
+                <el-option v-for="type in types" :key="type.value" :label="type.label" :value="type.value"></el-option>
+            </el-select>
+        </el-divider>
         <div class="m-celebrity-content">
             <div class="u-table-header">
                 <div class="u-row">
@@ -19,13 +24,13 @@
                             <i v-if="!i" class="u-icon"></i>
                             <span>{{ item.timeFormat }}</span>
                         </div>
-                        <div class="u-item">{{ item.map + "·" + item.site }}</div>
+                        <div class="u-item" v-if="type === '1'">
+                            <template v-if="item.key === 'y8'"> 特殊事件 · </template>
+                            {{ item.site }}
+                        </div>
+                        <div class="u-item" v-else>{{ item.map + "·" + item.site }}</div>
                         <div class="u-item">
-                            <span
-                                ><img :src="require(`@/assets/img/icon/minimap_${item.icon || 6}.png`)" />{{
-                                    item.stage
-                                }}</span
-                            >
+                            <span><img :src="`${iconPath}/minimap_${item.icon}.png`" />{{ item.stage }}</span>
                         </div>
                     </div>
                 </el-tooltip>
@@ -37,6 +42,8 @@
 // import { getCelebrities } from "@/service/node";
 import { getCelebrities } from "@/service/cms";
 import dayjs from "@/utils/day";
+import { sortBy } from "@/utils/index";
+const JX3BOX = require("@jx3box/jx3box-common/data/jx3box.json");
 export default {
     name: "celebrity",
     data: function () {
@@ -46,9 +53,23 @@ export default {
             showNum: 3,
             celebrityList: [],
             currentDate: {
-                h: dayjs.tz().hour(),
-                m: dayjs.tz().minute(),
+                // h: dayjs.tz().hour(),
+                // m: dayjs.tz().minute(),
+                h: 18,
+                m: 40,
             },
+            type: "1",
+            types: [
+                {
+                    label: "云从社",
+                    value: "1",
+                },
+                {
+                    label: "楚天社",
+                    value: "0",
+                },
+            ],
+            iconPath: JX3BOX.__imgPath + "pve/minimap",
         };
     },
     computed: {
@@ -57,9 +78,13 @@ export default {
         },
     },
     methods: {
-        loadData: function () {
+        typeChange(type) {
+            this.type = type;
+            this.loadData();
+        },
+        loadData() {
             this.loading = true;
-            return getCelebrities()
+            return getCelebrities({ type: this.type })
                 .then((res) => {
                     const data = res.data?.data || [];
                     this.celebrityList = data.map((item) => {
@@ -84,7 +109,8 @@ export default {
             const formatM = m.toString().padStart(2, "00");
             return `${h}:${formatM}`.padStart(5, "00:00");
         },
-        getList(date) {
+        // 处理楚天社
+        getChu(date) {
             const currentKey = "c" + (date.h % 2 === 0 ? "0" : "1") + (date.m < 30 ? "0" : "1");
             const isEqualMinute = this.celebrityList.findIndex((item) => {
                 return item.key === currentKey && item.time === date.m;
@@ -134,6 +160,95 @@ export default {
                 return item;
             });
         },
+        // 处理云从社
+        getYun(date) {
+            // 循环事件
+            const circleList = this.celebrityList.filter((item) => item.key !== "y8");
+            const currentKey = "y" + (date.h % 2 === 0 ? "0" : "1");
+            const isEqualMinute = circleList.findIndex((item) => {
+                return item.key === currentKey && item.time === date.m;
+            });
+            let index = 0;
+            if (isEqualMinute !== -1) {
+                // 包含当前时间
+                index = isEqualMinute;
+            } else {
+                // 不包含
+                const nIndex = circleList.findIndex((item) => {
+                    return item.key === currentKey && item.time > date.m;
+                });
+                if (nIndex === -1) {
+                    // 当前key中最后一个
+                    index = circleList.findLastIndex((item) => {
+                        return item.key === currentKey;
+                    });
+                } else {
+                    index = nIndex - 1;
+                }
+            }
+            let list = circleList.slice(index, index + this.showNum);
+            let newList = [];
+            if (list.length < this.showNum) {
+                newList = list.concat(circleList.slice(0, this.showNum - list.length));
+            } else {
+                newList = [].concat(list);
+            }
+
+            // 8小时cd事件
+            const y8List = this.celebrityList.filter((item) => item.key === "y8");
+            const y8FormatList = [];
+            for (let i = 0; i < 24; i += 8) {
+                y8List.forEach((item) => {
+                    y8FormatList.push({
+                        ...item,
+                        id: item.id + "" + item.hour + i,
+                        timeFormat: `${item.hour + i < 24 ? item.hour + i : item.hour + i - 24}:${item.time}`,
+                        h: item.hour + i < 24 ? item.hour + i : item.hour + i - 24,
+                        m: item.time,
+                    });
+                });
+            }
+            const sortY8FormatList = y8FormatList.sort(sortBy("h", "m"));
+            // 将要发生的最近的8小时CD事件
+            const latelyY8 = sortY8FormatList
+                .filter((item) => {
+                    return item.h >= date.h;
+                })
+                .find((item) => item.h > date.h || item.m >= date.m);
+            const circleFormatList = newList.map((item) => {
+                // 当前时间
+                let h = this.currentDate.h;
+                if (currentKey !== item.key) {
+                    h = h + 1;
+                }
+                item.h = h;
+                item.timeFormat = this.toFormatTime(h, item.time);
+                return item;
+            });
+            // 最近8小时事件在显示循环中的时间位置
+            let y8InIndex = -1;
+            if (latelyY8) {
+                y8InIndex = newList
+                    .filter((item) => item.h === latelyY8.h)
+                    .findIndex((item) => {
+                        return latelyY8.m > item.time;
+                    });
+            }
+            console.log(sortY8FormatList);
+            console.log(newList, latelyY8);
+            if (y8InIndex !== -1) {
+                circleFormatList.splice(y8InIndex, 1, latelyY8);
+            }
+            console.log(y8InIndex);
+            this.list = circleFormatList;
+        },
+        getList(date) {
+            if (this.type === "0") {
+                this.getChu(date);
+            } else {
+                this.getYun(date);
+            }
+        },
     },
     watch: {
         currentDate: {
@@ -150,14 +265,14 @@ export default {
         this.loadData();
     },
     mounted() {
-        setInterval(() => {
-            if (this.currentDate.h !== dayjs.tz().hour() || this.currentDate.m !== dayjs.tz().minute()) {
-                this.currentDate = {
-                    h: dayjs.tz().hour(),
-                    m: dayjs.tz().minute(),
-                };
-            }
-        }, 1000);
+        // setInterval(() => {
+        //     if (this.currentDate.h !== dayjs.tz().hour() || this.currentDate.m !== dayjs.tz().minute()) {
+        //         this.currentDate = {
+        //             h: dayjs.tz().hour(),
+        //             m: dayjs.tz().minute(),
+        //         };
+        //     }
+        // }, 1000);
     },
 };
 </script>
