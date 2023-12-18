9ef204e6360d18163f9287640c45c3124a450c6e
litthy <litthy@qq.com>
2023-12-18 09:59:10 +0800

    feat: 2023圣诞贺卡

--- src/components/festival/christmas.vue
+++ src/components/festival/christmas.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="christmas" @click.stop>
         <div class="christmas-card" @click="change">
-            <img v-if="!show" class="christmas-card__img" :src="`${imgLink}/christmas/cover.jpg`" alt="圣诞节快乐" />
-            <img v-if="!show" class="christmas-card__open" :src="`${imgLink}/christmas/button.png`" alt="圣诞节快乐" />
-            <img
-                v-if="show"
-                @click.stop="close"
-                class="animation christmas-card__content christmas-card__img"
-                :class="show ? 'flipInY' : ''"
-                :src="`${imgLink}/christmas/${fontCount || 0}.png`"
-                alt="圣诞节快乐"
-            />
+            <template v-if="!show">
+                <img class="christmas-card__img" :src="`${imgLink}/christmas2023/cover.jpg`" alt="圣诞节快乐" />
+                <img class="christmas-card__open" :src="`${imgLink}/christmas2023/button.png`" alt="圣诞节快乐" />
+            </template>
+            <div
+                v-else
+                :style="`background:url(${imgLink}/christmas2023/bg.jpg) top center no-repeat;background-size: cover;`"
+            >
+                <img
+                    @click.stop="close"
+                    class="animation christmas-card__content christmas-card__img"
+                    :class="show ? 'flipInY' : ''"
+                    :src="`${imgLink}/christmas2023/${fontCount || 0}.png`"
+                    alt="圣诞节快乐"
+                />
+            </div>
         </div>
     </div>
 </template>
