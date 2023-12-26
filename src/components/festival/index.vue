07ea2d32cd4197d98cec20d572a80b8fb71c4471
litthy <litthy@qq.com>
2023-12-26 15:50:24 +0800

    feat: 元旦贺卡

--- src/components/festival/index.vue
+++ src/components/festival/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-index-popup" v-if="success" v-show="visible" @click="close">
         <!-- 贺卡 -->
-        <christmas :fontCount="count" @checked="checked" @close="close" />
+        <newYearsDay :fontCount="count" @checked="checked" @close="close" />
     </div>
 </template>
 
@@ -9,7 +9,7 @@
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 import { getBoxCoin } from "@/service/index";
 import User from "@jx3box/jx3box-common/js/user";
-import christmas from "@/components/festival/christmas.vue";
+import newYearsDay from "@/components/festival/newYearsDay.vue";
 export default {
     name: "Festival",
     data: function () {
@@ -22,7 +22,7 @@ export default {
             check: false,
         };
     },
-    components: { christmas },
+    components: { newYearsDay },
     computed: {
         imgLink() {
             return __imgPath + `topic/festival/`;
