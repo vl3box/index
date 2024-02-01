af4dcf9a4ea857bf011ab44821e58e2cfb1b7663
litthy <litthy@qq.com>
2024-02-01 18:25:17 +0800

    feat: springFestival

--- src/components/festival/index.vue
+++ src/components/festival/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-index-popup" v-if="success" v-show="visible" @click="close">
         <!-- 贺卡 -->
-        <newYearsDay :fontCount="count" @checked="checked" @close="close" />
+        <springFestival :fontCount="count" @checked="checked" @close="close" />
     </div>
 </template>
 
@@ -9,7 +9,7 @@
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 import { getBoxCoin } from "@/service/index";
 import User from "@jx3box/jx3box-common/js/user";
-import newYearsDay from "@/components/festival/newYearsDay.vue";
+import springFestival from "@/components/festival/springFestival.vue";
 export default {
     name: "Festival",
     data: function () {
@@ -22,7 +22,7 @@ export default {
             check: false,
         };
     },
-    components: { newYearsDay },
+    components: { springFestival },
     computed: {
         imgLink() {
             return __imgPath + `topic/festival/`;
--- src/components/festival/springFestival.vue
+++ src/components/festival/springFestival.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="m-springFestival" :class="closeType ? 'u-card-close' : ''" @click.stop>
-        <div class="u-springFestival-card" :class="show ? 'u-card-on' : 'u-card-off'" @click="change">
-            <img class="u-card-bg" :src="`${imgLink}/springFestival/BG.jpg`" alt="新年快乐" />
-            <img class="u-card-cover" :src="`${imgLink}/springFestival/cover.png`" alt="新年快乐" />
-            <img class="u-card-button" :src="`${imgLink}/springFestival/button.png`" alt="新年快乐" />
-            <img class="u-card-content" :src="`${imgLink}/springFestival/${fontCount}.png`" alt="新年快乐" />
+    <div class="m-springFestival" @click.stop>
+        <div class="u-springFestival-card fadeInUpBig p-animation">
+            <div class="u-letter u-img m-animation" ref="letter" @click="close">
+                <img class="u-img" :src="`${imgLink}/springFestival2024/letter.png`" alt="新年快乐" />
+                <img class="u-img" :src="`${imgLink}/springFestival2024/${fontCount}.png`" alt="新年快乐" />
+            </div>
+            <img
+                class="u-cover u-img"
+                ref="cover"
+                :src="`${imgLink}/springFestival2024/cover.png`"
+                alt="新年快乐"
+                @click="open"
+            />
         </div>
     </div>
 </template>
@@ -19,21 +26,72 @@ export default {
         },
     },
     data() {
-        return {
-            show: false,
-            closeType: false,
-        };
+        return {};
     },
     methods: {
-        change() {
-            if (this.show) {
-                this.closeType = true;
-                setTimeout(() => {
-                    this.$emit("close");
-                }, 1000);
-            }
-            this.show = !this.show;
+        open() {
+            const letter = this.$refs.letter;
+            const cover = this.$refs.cover;
+            cover.classList.add("click");
+            letter.classList.add("fadeOutDownBig");
+            letter.addEventListener(
+                "animationend",
+                () => {
+                    letter.classList.remove("fadeOutDownBig");
+                    letter.classList.add("fadeInDownBig");
+                },
+                { once: true }
+            );
+        },
+        close() {
+            this.$emit("close");
         },
     },
 };
 </script>
+<style lang="less">
+@import "../../assets/css/animation.less";
+.m-animation {
+    -webkit-animation-duration: 0.5s;
+    animation-duration: 0.5s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+}
+.m-springFestival {
+    .pf;
+    .full;
+    .lt(0);
+    .u-springFestival-card {
+        .pointer;
+        .size(392px,720px);
+        .ct(o,392px,720px);
+        .u-img {
+            .pa;
+            .full;
+            .lt(0);
+            transition: all 0.5s;
+        }
+        &:hover {
+            .u-letter {
+                top: 5%;
+            }
+        }
+        .u-letter.fadeInDownBig {
+            .z(1);
+            top: 0 !important;
+        }
+        .u-cover.click {
+            transition: all 1.5s;
+            transform: rotate(5deg);
+        }
+    }
+}
+@media screen and (min-width: @2k) {
+    .m-springFestival {
+        .u-springFestival-card {
+            .size(600px,1100px);
+            .ct(o,600px,1100px);
+        }
+    }
+}
+</style>
