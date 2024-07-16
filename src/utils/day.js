746bd25fbc1a98327474abc3b7f32c70d0e8aab9
fifththirteen <570205219@qq.com>
2024-07-16 13:01:42 +0800

    fix:设置用户当前时区

--- src/utils/day.js
+++ src/utils/day.js
@@ -4,9 +4,9 @@ import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import isToday from "dayjs/plugin/isToday";
 import isoWeek from "dayjs/plugin/isoWeek";
-import isBetween from "dayjs/plugin/isBetween"
-import isSameOrAfter from "dayjs/plugin/isSameOrAfter"
-import isSameOrBefore from "dayjs/plugin/isSameOrBefore"
+import isBetween from "dayjs/plugin/isBetween";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(isoWeek);
@@ -16,6 +16,8 @@ dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
 
 import store from "@/store";
+// 用户当前时区
+store.state.timezone = dayjs.tz.guess();
 const _timezone = store.state.timezone;
 dayjs.tz.setDefault(_timezone);
 
